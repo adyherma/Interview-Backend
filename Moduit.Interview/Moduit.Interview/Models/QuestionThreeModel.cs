4387482270c0ada5478324a4ebd17030a2b27b82
Ady Hermawan <ady.hermawan@moonlay.com>
2021-08-27 14:30:23 +0700

    Create Model

---
+++ Moduit.Interview/Moduit.Interview/Models/QuestionThreeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moduit.Interview.Models
+{
+    public class QuestionThreeModel : BaseModel
+    {
+        public string Category { get; set; }
+    }
+
+    public class QuestionThreeVM : BaseModel
+    {
+        public string Category { get; set; }
+        public List<BaseModel> Items { get; set; }
+    }
+}
