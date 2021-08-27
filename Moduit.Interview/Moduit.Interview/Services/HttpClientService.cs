879a7dfd6ccc5624d39b854e447eac349b1a766c
Ady Hermawan <ady.hermawan@moonlay.com>
2021-08-27 14:31:31 +0700

    Create Service

---
+++ Moduit.Interview/Moduit.Interview/Services/HttpClientService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Moduit.Interview.Services
+{
+    public class HttpClientService : IHttpClientService
+    {
+        private HttpClient _client = new HttpClient();
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await _client.GetAsync(url);
+        }
+    }
+
+    public interface IHttpClientService
+    {
+        Task<HttpResponseMessage> GetAsync(string url);
+    }
+}
