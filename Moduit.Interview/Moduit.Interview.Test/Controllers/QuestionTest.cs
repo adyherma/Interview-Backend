0d5228b60515c7dd84e018ad025b69df9a903efe
Ady Hermawan <ady.hermawan@moonlay.com>
2021-08-27 14:30:53 +0700

    Create Unit Test

---
+++ Moduit.Interview/Moduit.Interview.Test/Controllers/QuestionTest.cs
@@ -0,0 +1,86 @@
+﻿using Moduit.Interview.Controllers;
+using Moduit.Interview.Models;
+using Moduit.Interview.Services;
+using Moduit.Interview.Test.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Moduit.Interview.Test.Controllers
+{
+    public class QuestionTest
+    {
+        [Fact]
+        public async Task Should_Success_GetQuestionOne()
+        {
+            var serviceProvider = new Mock<IServiceProvider>();
+
+            serviceProvider
+                .Setup(x => x.GetService(typeof(IHttpClientService)))
+                .Returns(new HttpClientServiceTest());
+
+            var service = new QuestionController(serviceProvider.Object);
+            var result = await service.GetQuestionOne();
+            Assert.Equal((int)HttpStatusCode.OK, result.GetType().GetProperty("StatusCode").GetValue(result, null));
+        }
+
+        [Fact]
+        public async Task Should_Success_GetQuestionTwo()
+        {
+            var serviceProvider = new Mock<IServiceProvider>();
+
+            serviceProvider
+                .Setup(x => x.GetService(typeof(IHttpClientService)))
+                .Returns(new HttpClientServiceQuestionTwoTest());
+            var service = new QuestionController(serviceProvider.Object);
+            var result = await service.GetQuestionTwo();
+            Assert.Equal((int)HttpStatusCode.OK, result.GetType().GetProperty("StatusCode").GetValue(result, null));
+        }
+
+        [Fact]
+        public async Task Should_Success_GetQuestionThree()
+        {
+            var serviceProvider = new Mock<IServiceProvider>();
+
+            serviceProvider
+                .Setup(x => x.GetService(typeof(IHttpClientService)))
+                .Returns(new HttpClientServiceQuestionThreeTest());
+
+            var service = new QuestionController(serviceProvider.Object);
+            var result = await service.GetQuestionThree();
+            Assert.Equal((int)HttpStatusCode.OK, result.GetType().GetProperty("StatusCode").GetValue(result, null));
+        }
+
+        [Fact]
+        public async Task Should_Failed_GetQuestionTwo()
+        {
+            var serviceProvider = new Mock<IServiceProvider>();
+
+            serviceProvider
+                .Setup(x => x.GetService(typeof(IHttpClientService)))
+                .Returns(new HttpClientServiceTest());
+
+            var service = new QuestionController(serviceProvider.Object);
+            Exception message = await Assert.ThrowsAnyAsync<Exception>(() => service.GetQuestionTwo());
+            Assert.Equal("Data not found", message.Message);
+        }
+
+
+        [Fact]
+        public async Task Should_Failed_GetQuestionThree()
+        {
+            var serviceProvider = new Mock<IServiceProvider>();
+
+            serviceProvider
+                .Setup(x => x.GetService(typeof(IHttpClientService)))
+                .Returns(new HttpClientServiceTest());
+
+            var service = new QuestionController(serviceProvider.Object);
+            Exception message = await Assert.ThrowsAnyAsync<Exception>(() => service.GetQuestionThree());
+            Assert.Equal("Data not found", message.Message);
+        }
+    }
+}
---
+++ Moduit.Interview/Moduit.Interview.Test/Services/HttpClientServiceTest.cs
@@ -0,0 +1,87 @@
+﻿using Moduit.Interview.Models;
+using Moduit.Interview.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moduit.Interview.Test.Services
+{
+    public class HttpClientServiceTest : IHttpClientService
+    {
+        public Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return Task.Run(() => new HttpResponseMessage());
+        }
+    }
+
+    public class HttpClientServiceQuestionTwoTest : IHttpClientService
+    {
+        public Task<HttpResponseMessage> GetAsync(string url)
+        {
+            var rnd = new Random();
+            var guid = Guid.NewGuid().ToString("N").Substring(0, 10);
+            var listData = new List<QuestionTwoModel>();
+            List<string> tags = new List<string>(new string[] { $"Ergonomic {guid}"});
+            var model = new QuestionTwoModel()
+            {
+                Id = rnd.Next(1000, 4000),
+                CreatedAt = DateTime.Now,
+                Description = "Ergonomic " + guid,
+                Footer = guid,
+                Title = guid,
+                Tags = tags
+            };
+
+            listData.Add(model);
+
+            var jsonStringData = JsonConvert.SerializeObject(listData);
+            var responseMessage = new HttpResponseMessage();
+            responseMessage.Content = new StringContent(jsonStringData);
+
+            return Task.Run(() => responseMessage);
+        }
+    }
+
+    public class HttpClientServiceQuestionThreeTest : IHttpClientService
+    {
+        public Task<HttpResponseMessage> GetAsync(string url)
+        {            
+            var rnd = new Random();
+            var guid = Guid.NewGuid().ToString("N").Substring(0, 10);
+            var items = new List<BaseModel>();
+            var listData = new List<QuestionThreeVM>();
+
+            items.Add(new BaseModel()
+            {
+                Id = rnd.Next(1000, 4000),
+                CreatedAt = DateTime.Now,
+                Description = guid,
+                Footer = guid,
+                Title = guid,
+            });
+
+            var model = new QuestionThreeVM()
+            {
+                Id = rnd.Next(1000, 4000),
+                Category = guid,
+                CreatedAt = DateTime.Now,
+                Description = guid,
+                Footer = guid,
+                Title = guid,
+                Items = items
+            };
+
+            listData.Add(model);
+
+            var jsonStringData = JsonConvert.SerializeObject(listData);
+            var responseMessage = new HttpResponseMessage();
+            responseMessage.Content = new StringContent(jsonStringData);
+
+            return Task.Run(() => responseMessage);
+        }
+    }
+}
